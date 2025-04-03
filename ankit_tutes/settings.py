2d75ae4e49c02c035acef96681ef06b7152b7f79
gaurav.singh2 <gaurav.singh2@anaplan.com>
2025-04-03 23:39:18 +0530

    hggkjhk

--- ankit_tutes/settings.py
+++ ankit_tutes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nt^*p2)5tz)at@!db%+!(7%kze&21#8r1x-=we&%#jl^n^*_jx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
