8ca3f979de04148bb87bb67adcc1afc03bba5190
gaurav.singh2 <gaurav.singh2@anaplan.com>
2025-04-03 23:54:03 +0530

    dsfgdg

--- ankit_tutes/views.py
+++ ankit_tutes/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 
 def welcome(request):
     print("hello")
-    return HttpResponse("Hello welcome to anshu project")
-
-def ishu(request):
-    print("Jagdish ki maka bhosda")
-    return HttpResponse("puppu ki maka bhosda")
+    return HttpResponse("Hello welcome to ankit project")
